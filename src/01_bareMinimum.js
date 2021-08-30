5699f1831545e60545497e4c74a24da7f5fc3442
yoonmi-cyber <yoonmi1126@gmail.com>
2021-08-30 15:48:56 +0900

    ym4

--- src/01_bareMinimum.js
+++ src/01_bareMinimum.js
@@ -324,7 +324,11 @@ _.uniq = function (arr) {
 // _.map은 iteratee(반복되는 작업)를 배열의 각 요소에 적용(apply)한 결과를 담은 새로운 배열을 리턴합니다.
 // 함수의 이름에서 드러나듯이 _.map은 배열의 각 요소를 다른 것(iteratee의 결과)으로 매핑(mapping)합니다.
 _.map = function (arr, iteratee) {
-  // TODO: 여기에 코드를 작성합니다.
+  let result = []
+  _.each(arr, function(el){
+    result.push(iteratee(el))
+  })
+  return result
   // _.map 함수는 매우 자주 사용됩니다.
   // _.each 함수와 비슷하게 동작하지만, 각 요소에 iteratee를 적용한 결과를 리턴합니다.
 };
